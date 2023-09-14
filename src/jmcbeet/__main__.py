510c9e8be331c5a423eb7d0b0868aa1f3621e948
WingedSeal <firm09719@gmail.com>
2023-09-14 16:12:56 +0700

    fix: fix minor bugs

--- src/jmcbeet/__main__.py
+++ src/jmcbeet/__main__.py
@@ -47,7 +47,7 @@ def main():
             namespace = input("namespace: ")
 
     if not (cwd / "beet.yaml").is_file():
-        with (cwd / "beet.yaml").open("wr") as file:
+        with (cwd / "beet.yaml").open("w+") as file:
             file.write(get_beet_yaml(namespace))
 
     namespace_folder = cwd / "src" / "data" / namespace
@@ -57,8 +57,6 @@ def main():
 
     is_in_venv = sys.prefix != sys.base_prefix
     if not is_in_venv:
-        window_activate = r"`venv\Scripts\activate`"
-        unix_activate = r"`source venv\bin\activate`"
         subprocess.check_call([sys.executable, "-m", "venv", "venv"])
         if os.name == "nt":
             subprocess.check_call([r"venv\Scripts\activate"])
