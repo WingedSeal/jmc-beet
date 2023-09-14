33c2ff76fc92130a34981c6fccdea5bdf1eb2dcc
WingedSeal <firm09719@gmail.com>
2023-09-14 16:35:34 +0700

    feat: imporve upload.bat

--- upload.bat
+++ upload.bat
@@ -3,6 +3,6 @@ del jmc-beet* /a
 cd ..
 python setup.py sdist bdist_wheel 
 START /B /wait cmd /c "twine upload dist/jmc-beet*"
-rmdir /s jmc_beet.egg-info
-rmdir /s build
+rmdir /s /Q jmc_beet.egg-info
+rmdir /s /Q build
 cd ..
